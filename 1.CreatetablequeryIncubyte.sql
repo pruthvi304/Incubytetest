8b24c0ca70088c11da78a989b44700a1da9620d2
pruthvi304 <61644759+pruthvi304@users.noreply.github.com>
2021-05-07 18:13:10 +0530

    Update 1.CreatetablequeryIncubyte.sql

--- 1.CreatetablequeryIncubyte.sql
+++ 1.CreatetablequeryIncubyte.sql
@@ -4,7 +4,7 @@ create table incubyte(
 	Cust_Name varchar(255) NOT NULL,
 	Cust_ID varchar(18) NOT NULL,
 	Open_Date date NOT NULL,
-	Last_Cunsult_Date date,
+	Last_Consult_Date date,
 	Vaccination_ID char(5),
 	Dr_Name varchar(255),
 	State char(5),
@@ -13,7 +13,7 @@ create table incubyte(
 	DOB date,
 	Is_Active char(1),
 	primary key(Cust_Name)
-(
+)
 insert into incubyte values('Alex','123457','2010-10-12','2012-10-13', 'MVD', 'Paul','SA','USA',123456,'1987-06-03','A')
 insert into incubyte values('Pruthvi','123458','2010-10-12','2012-10-13', 'MVD', 'Paul','TS','IND',500079,'1987-06-03','A')
 insert into incubyte values('John','123459','2010-10-12','2012-10-13', 'MVD', 'Paul','VIC','AUS',564651,'1987-06-03','A')
